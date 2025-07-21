ca372f25c0e463b6ffc824056541632a09dc5fc8
David Kurniadi Angdinata <dka31@cantab.ac.uk>
2025-07-21 17:01:06 +0100

    Prove O[K] is a DVR

--- ClassFieldTheory/LubinTateTheory/Basic.lean
+++ ClassFieldTheory/LubinTateTheory/Basic.lean
@@ -4,10 +4,16 @@ import Mathlib.Topology.Algebra.Field
 import Mathlib.Topology.Algebra.Valued.ValuativeRel
 import Mathlib.Topology.UniformSpace.Cauchy
 import Mathlib.Topology.UniformSpace.Defs
+import Mathlib.NumberTheory.Padics.ProperSpace
+import Mathlib.Topology.Algebra.Valued.LocallyCompact
+
+open ValuativeRel
 
 universe u
 
-class IsNonArchLF (K : Type u) [Field K] [ValuativeRel K] [UniformSpace K] : Prop extends
+variable (K : Type u) [Field K] [ValuativeRel K] [UniformSpace K]
+
+class IsNonArchLF : Prop extends
   IsTopologicalDivisionRing K,
   LocallyCompactSpace K,
   CompleteSpace K,
@@ -16,9 +22,21 @@ class IsNonArchLF (K : Type u) [Field K] [ValuativeRel K] [UniformSpace K] : Pro
   ValuativeRel.IsRankLeOne K,
   ValuativeRel.IsDiscrete K
 
-variable (K : Type u) [Field K] [ValuativeRel K] [UniformSpace K]
+variable [IsNonArchLF K]
 
-open scoped ValuativeRel
+instance : IsUniformAddGroup K where
+  uniformContinuous_sub := sorry
+
+instance : (Valued.v : Valuation K (ValueGroupWithZero K)).IsNontrivial :=
+  ValuativeRel.isNontrivial_iff_isNontrivial.mp inferInstance
+
+noncomputable instance : (Valued.v : Valuation K (ValueGroupWithZero K)).RankOne where
+  hom := IsRankLeOne.nonempty.some.emb
+  strictMono' := IsRankLeOne.nonempty.some.strictMono
+
+open scoped Valued in
+instance : ProperSpace K :=
+  ProperSpace.of_nontriviallyNormedField_of_weaklyLocallyCompactSpace K
 
 instance : IsDiscreteValuationRing 𝒪[K] :=
-  sorry
+  (Valued.integer.properSpace_iff_completeSpace_and_isDiscreteValuationRing_integer_and_finite_residueField.mp inferInstance).2.1
