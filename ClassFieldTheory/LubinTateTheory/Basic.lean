fa28cd16b783379fe5582b512dc702f612b717ec
WenrongZou <wenrongzou@outlook.com>
2025-07-21 20:14:36 +0200

    Formalize of first lemma

--- ClassFieldTheory/LubinTateTheory/Basic.lean
+++ ClassFieldTheory/LubinTateTheory/Basic.lean
@@ -9,7 +9,7 @@ import Mathlib.Topology.Algebra.Valued.LocallyCompact
 import Mathlib.RingTheory.PowerSeries.Substitution
 import Mathlib.RingTheory.PowerSeries.Trunc
 
-open ValuativeRel PowerSeries
+open ValuativeRel MvPowerSeries
 
 universe u
 
@@ -46,11 +46,42 @@ instance : IsDiscreteValuationRing 𝒪[K] :=
 
 variable (π : 𝒪[K])
 
-include K in
-def residue_size  : ℕ := sorry
+instance : Fintype (𝒪[K] ⧸ (IsLocalRing.maximalIdeal (𝒪[K]))) := by
 
-structure F  where
+  sorry
+
+include K in
+def residue_size : ℕ := Fintype.card (𝒪[K] ⧸ (IsLocalRing.maximalIdeal (𝒪[K])))
+
+
+structure LubinTateF  where
 toFun : PowerSeries 𝒪[K]
 trunc_degree_two : PowerSeries.trunc 2 toFun = (Polynomial.C π) * Polynomial.X
-mod_pi : toFun.coeff _ residue_size ≡ 1 [SMOD (IsLocalRing.maximalIdeal (𝒪[K]))]
-  ∧ ∀ n, n ≠ residue_size →  toFun.coeff _ n ≡ 0 [SMOD (IsLocalRing.maximalIdeal (𝒪[K]))]
+mod_pi : toFun.coeff (𝒪[K]) (residue_size K) ≡ 1 [SMOD (IsLocalRing.maximalIdeal (𝒪[K]))]
+  ∧ ∀ n, n ≠ (residue_size K) →  toFun.coeff _ n ≡ 0 [SMOD (IsLocalRing.maximalIdeal (𝒪[K]))]
+
+noncomputable section
+
+variable {σ : Type*} [DecidableEq σ] [Finite σ] {R : Type*} [CommRing R]
+
+
+abbrev deg_map (n : ℕ) : σ →₀ ℕ :=
+  Finsupp.equivFunOnFinite.invFun (fun _ => n)
+
+abbrev subst_aux (f : PowerSeries R) : σ → MvPowerSeries σ R :=
+  (fun i => PowerSeries.subst (X i) f)
+
+def MvPowerSeries.truncDegFun (n : ℕ) (φ : MvPowerSeries σ R) : MvPolynomial σ R :=
+  MvPowerSeries.truncFun (deg_map n) φ
+
+variable (R) in
+def MvPowerSeries.trunc_deg (n : ℕ) := trunc R (deg_map n) (σ := σ)
+
+
+theorem constructive_lemma (n : ℕ) {ϕ₁ : MvPowerSeries (Fin n) 𝒪[K]}
+  {a : Fin n → 𝒪[K]}
+  (h_phi₁ : ϕ₁ = ∑ (i : Fin n), (C (Fin n) 𝒪[K] (a i)) * X i)
+  (f g : LubinTateF K π)
+  :
+  ∃! (ϕ : MvPowerSeries (Fin n) 𝒪[K]), trunc_deg (𝒪[K]) 2 ϕ = ϕ₁ ∧
+  PowerSeries.subst ϕ f.toFun = subst (subst_aux g.toFun) ϕ := sorry
